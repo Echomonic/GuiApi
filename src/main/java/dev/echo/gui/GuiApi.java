19bb3ea58ddfd6a6069292cab188391c264fcbe5
ethom <lineaox@gmail.com>
2020-12-23 10:53:37 -0900

    Initial commit

---
+++ src/main/java/dev/echo/gui/GuiApi.java
@@ -0,0 +1,15 @@
+package dev.echo.gui;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GuiApi extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
---
+++ src/main/java/dev/echo/gui/createGui.java
@@ -0,0 +1,47 @@
+package dev.echo.gui;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.event.Listener;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
+import org.bukkit.inventory.ItemStack;
+
+public class createGui implements Listener {
+
+    private Inventory apiInv;
+
+
+    public Inventory createGui(InventoryHolder owner, int slots, String title, boolean isPaginated){
+
+       apiInv = Bukkit.createInventory(owner,slots, ChatColor.translateAlternateColorCodes('&',title));
+
+       return apiInv;
+    }
+
+    public void addItem(ItemStack stack, int i){
+        apiInv.setItem(i, stack);
+    }
+
+    public void setEntireInventory(ItemStack stack){
+
+        for(int i = 0; i < apiInv.getSize(); i++){
+            if(apiInv.getItem(i) == null){
+                apiInv.setItem(i, stack);
+            }
+        }
+
+    }
+    public void removeItem(ItemStack stack){
+
+        apiInv.remove(stack);
+
+    }
+
+    public void getSlot(int i){
+
+        apiInv.getItem(i);
+    }
+
+
+}
