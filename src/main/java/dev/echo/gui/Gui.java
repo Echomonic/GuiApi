66d040ed238f4cfadcb7c934ece1329cfb58a37d
ethom <lineaox@gmail.com>
2020-12-23 11:09:59 -0900

    Changed A Class

--- src/main/java/dev/echo/gui/Gui.java
+++ src/main/java/dev/echo/gui/Gui.java
@@ -7,7 +7,7 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
 
-public class createGui implements Listener {
+public class Gui implements Listener {
 
     private Inventory apiInv;
 
